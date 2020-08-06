e34cabd1c627f30268ef6d620be11443da7eb28a
Mariana Barreiros <mariana.barreiros@dasa.com.br>
2020-08-06 02:04:06 -0300

    Initialize the project.

---
+++ src/main/java/br/com/dasa/laboratorymanage/LaboratoryManageApplication.java
@@ -0,0 +1,13 @@
+package br.com.dasa.laboratorymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dasa/laboratorymanage/LaboratoryManageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dasa.laboratorymanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
